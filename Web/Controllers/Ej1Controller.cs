eee7a4d8be670d80b0ed2a0087c4fd473ddd600e
efalabrini <edfsoft@gmail.com>
2024-03-24 23:35:49 -0300

    First commit

---
+++ Web/Controllers/Ej1Controller.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej1Controller : ControllerBase
+    {
+        [HttpGet()]
+        public int Get([FromQuery] int var1, [FromQuery] int var2, [FromQuery] int var3)
+        {
+            
+            int result = var1 + var2 + var3;
+            return result;
+        }
+    }
+}
