ddad7bfde2eb63fba9ea2b7d8d1249466dd9a764
Augusto-Camani <augustocamani@gmail.com>
2024-03-26 15:09:13 -0300

    Ejercicio 2/ String interpolation.

---
+++ Web/Controllers/Ej2Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej2Controller : ControllerBase
+    {
+        [HttpGet()]
+        public string Get([FromQuery] string name, [FromQuery] string city)
+        {
+            string greeting = $"Hola {name} bienvenido a {city}.";
+            return greeting;
+        }
+    }
+}
