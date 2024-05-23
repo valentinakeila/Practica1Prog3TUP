073bfcc1e82eb263f9cae4c835d7e571a2d0ef8c
Valen <valentinakeila12@gmail.com>
2024-05-23 00:39:24 -0300

    ej8

---
+++ Web/Controllers/Ej8Controller.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej8Controller : ControllerBase
+    {
+        [HttpGet()]
+        public int Get()
+        {
+            int num = 0;
+            while (num < 100)
+            {
+                return num + 1;
+            }
+        }
+    }
+}
+
+
+
+
+
