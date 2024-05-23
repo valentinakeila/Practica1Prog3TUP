3261a2ea58bf1bee274b3eb97730970d7a46cc82
Valen <valentinakeila12@gmail.com>
2024-05-23 01:06:13 -0300

    ej9

---
+++ Web/Controllers/Ej9Controller.cs
@@ -0,0 +1,25 @@
+﻿
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Ej9Controller : ControllerBase
+    {
+        [HttpGet()]
+        public List<int> Get()
+        {
+            List<int> lista = new List<int>();
+
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    lista.Add(i);
+                }
+            }
+            return lista;
+        }
+    }
+}
