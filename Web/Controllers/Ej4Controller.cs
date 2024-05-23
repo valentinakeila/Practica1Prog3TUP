2ea8dc464829141c208b7a3e1cb9e861a7f853a1
Valen <valentinakeila12@gmail.com>
2024-05-22 23:47:49 -0300

    ej4

---
+++ Web/Controllers/Ej4Controller.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+
+    public class Ej4Controller : ControllerBase
+    {
+        [HttpGet()]
+        public string Get([FromQuery] int number1, [FromQuery] int number2)
+        {
+            if (number1 > number2)
+            {
+                return $"El numero {number1} es mayor";
+            }
+            else if (number1 < number2)
+            {
+                return $"El numero {number2} es mayor";
+            }
+            else
+            {
+                return $"Ambos numeros son iguales";
+            }
+        }
+    }
+}
