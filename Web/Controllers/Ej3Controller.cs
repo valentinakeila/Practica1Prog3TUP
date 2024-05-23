44ee2dc987c21bd0c9aa9185df08fad081a5a058
Valen <valentinakeila12@gmail.com>
2024-05-22 23:38:55 -0300

    ej4

--- Web/Controllers/Ej3Controller.cs
+++ Web/Controllers/Ej3Controller.cs
@@ -3,80 +3,26 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers
 {
-    public class Ej3Controller : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+
+
+    public class Ej4Controller : ControllerBase
     {
-        // GET: Ej3Controller
-        public ActionResult Index()
+        [HttpGet()]
+        public string Get([FromQuery] int number1, [FromQuery] int number2)
         {
-            return View();
-        }
-
-        // GET: Ej3Controller/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Ej3Controller/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Ej3Controller/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
+            if (number1 > number2)
             {
-                return RedirectToAction(nameof(Index));
+                return $"El numero {number1} es mayor";
             }
-            catch
+            else if (number1 < number2)
             {
-                return View();
+                return $"El numero {number2} es mayor";
             }
-        }
-
-        // GET: Ej3Controller/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Ej3Controller/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
+            else
             {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Ej3Controller/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Ej3Controller/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
+                return $"Ambos numeros son iguales";
             }
         }
     }
