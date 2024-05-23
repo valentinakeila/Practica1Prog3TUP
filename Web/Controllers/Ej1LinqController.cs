d03be144056f4000424fc811bdd1b10a7b14c57d
Valen <valentinakeila12@gmail.com>
2024-05-23 10:24:25 -0300

    ej1linq

---
+++ Web/Controllers/Ej1LinqController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej1LinqController : ControllerBase
+    {
+        [HttpGet()]
+        public List<int> Get([FromQuery] List<int> oneList)
+        {
+            var filteredList = oneList.Where(x => x > 30 && x < 100).ToList();
+            return filteredList;
+
+
+        }
+    }
+}
